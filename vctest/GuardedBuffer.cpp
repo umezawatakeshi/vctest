77597db0af2d16ccccc03a7542399e5868fb8b9b
UMEZAWA Takeshi <umezawa@utsoft.org>
2020-01-10 21:38:22 +0900

    GuardedBuffer のコンストラクタで実際に OS に実メモリを割り当てさせるようにした。

--- vctest/GuardedBuffer.cpp
+++ vctest/GuardedBuffer.cpp
@@ -34,6 +34,14 @@ CGuardedBuffer::CGuardedBuffer(size_t cb, bool bHighAddress)
 	}
 	m_pGuarded = m_pAllocated + GUARDSIZE;
 
+	/*
+	 * VirtualAlloc 直後はアドレス空間が予約されているだけの状態のはずなので、
+	 * 計測中にページフォルトが発生して影響を受けるのを回避するために、
+	 * ここで書き込みを行って実際に実メモリを割り当てさせる。
+	 */
+	for (volatile char* p = m_pGuarded; p < m_pGuarded + m_cbGuarded; p += 4096)
+		*p = 0;
+
 	VirtualProtect(m_pAllocated, GUARDSIZE, PAGE_NOACCESS, &dw);
 	VirtualProtect(m_pGuarded + m_cbGuarded, GUARDSIZE, PAGE_NOACCESS, &dw);
 }
