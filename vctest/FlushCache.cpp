15093f8209ab1ef36b492e2975badf3814ec7bc7
UMEZAWA Takeshi <umezawa@utsoft.org>
2020-03-14 23:39:34 +0900

    Intel SDE 8.35.0 の CLFLUSH のバグを回避した。

--- vctest/FlushCache.cpp
+++ vctest/FlushCache.cpp
@@ -52,19 +52,25 @@ void FlushCache(const void* buf, size_t sz)
 	if (sz == 0)
 		return;
 
+	/*
+	 * Intel SDE 8.35.0 にはバグがあって、
+	 * CLFLUSH で指定したアドレスから 64 バイトアクセス可能でないとアクセス違反を起こすので、
+	 * アドレスを整列させて CLFLUSH する。
+	 */
 	const char* begin = (const char*)buf;
-	const char* end = begin + sz;
+	const char* end = begin + sz - 1;
+	begin = (const char*)((uintptr_t)begin & ~(uintptr_t)(cbStrideCLFLUSH - 1));
+	end   = (const char*)((uintptr_t)end   & ~(uintptr_t)(cbStrideCLFLUSH - 1));
+
 	if (bSupportCLFLUSHOPT)
 	{
-		for (auto p = begin; p < end; p += cbStrideCLFLUSH)
+		for (auto p = begin; p <= end; p += cbStrideCLFLUSH)
 			_mm_clflushopt(p);
-		_mm_clflushopt(end - 1);
 	}
 	else
 	{
-		for (auto p = begin; p < end; p += cbStrideCLFLUSH)
+		for (auto p = begin; p <= end; p += cbStrideCLFLUSH)
 			_mm_clflush(p);
-		_mm_clflush(end - 1);
 	}
 
 	_mm_sfence();
